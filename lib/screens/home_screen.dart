ec6804fb916b0cda53d6cb65458a8aea976c5c31
Florencia Farias <florencia.fariasmm@gmail.com>
2022-09-03 13:54:22 -0300

    Flor - construyendo App contador

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget{
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return  Scaffold(
+      appBar: AppBar(
+        title: const Text('HomeScreen'),
+        elevation: 10.0,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children:  const [
+            Text('Número de clicks'),
+            Text('2')
+          ]
+        ),
+      ),
+    );
+  }
+
+}
